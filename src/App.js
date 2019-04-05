34a0a1c097f1589e496d28641a82b7d292583573
Aaron Vanderpoel <aaron.vanderpoel@gmail.com>
2019-04-05 13:29:13 -0400

    ag-grid-styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled, { ThemeProvider, ThemeConsumer } from 'styled-components';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+
+const Container = styled.div`
+  color: ${props => props.theme.color};
+`;
+
+class CustomRenderer extends Component {
+  render() {
+    return (
+      <ThemeConsumer>
+        {theme => {
+          console.log('theme', theme);
+          return (<Container theme={theme}>{this.props.value}</Container>);
+        }}
+      </ThemeConsumer>
+    );
+  }
+}
+
+const gridOptions = {
+  frameworkComponents: {
+    customRenderer: CustomRenderer,
+  },
+  columnDefs: [
+    {
+      headerName: 'Custom Column',
+      field: 'myData',
+      cellRenderer: 'customRenderer',
+    },
+  ],
+  rowData: [
+    { myData: 'Color Missing' },
+  ]
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={{ color: 'blue' }}>
+        <div className="App">
+          <h1>Outside Grid:</h1>
+          <Container>Color Blue</Container>
+
+          <h1>Grid:</h1>
+          <AgGridReact {...gridOptions} />
+        </div>
+      </ThemeProvider>
     );
   }
 }
